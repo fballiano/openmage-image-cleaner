687184d3337df7a9482c71a06bec7046d2bf52ae
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-05-02 11:31:31 +0100

    Fixed null parameter warning

--- app/code/community/Fballiano/ImageCleaner/Helper/Data.php
+++ app/code/community/Fballiano/ImageCleaner/Helper/Data.php
@@ -1,23 +1,13 @@
 <?php
-
 /**
- * FBalliano
- *
- * NOTICE OF LICENSE
- *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
  *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade this Module to
- * newer versions in the future.
- *
  * @category   FBalliano
  * @package    FBalliano_ImageCleaner
- * @copyright  Copyright (c) 2021 Fabrizio Balliano (http://fabrizioballiano.it)
+ * @copyright  Copyright (c) Fabrizio Balliano (http://fabrizioballiano.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Fballiano_ImageCleaner_Helper_Data extends Mage_Core_Helper_Abstract
@@ -83,12 +73,17 @@ class Fballiano_ImageCleaner_Helper_Data extends Mage_Core_Helper_Abstract
         return $result;
     }
 
-    protected function getBlacklistedPatterns()
+    protected function getBlacklistedPatterns(): array
     {
-        return preg_split('/\r\n|\r|\n/', Mage::getStoreConfig('admin/fb_image_cleaner/blacklist'));
+        $blacklist = Mage::getStoreConfig('admin/fb_image_cleaner/blacklist');
+        if ($blacklist === null) {
+            return [];
+        }
+
+        return preg_split('/\r\n|\r|\n/', $blacklist);
     }
 
-    public function isBlacklisted($path, $blacklisted_patterns)
+    public function isBlacklisted($path, $blacklisted_patterns): bool
     {
         foreach ($blacklisted_patterns as $blacklisted_pattern) {
             if (fnmatch('*/' . $blacklisted_pattern, $path)) return true;
